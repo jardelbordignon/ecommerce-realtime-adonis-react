dc331f27f35cc1f5667cc3cd9196ee86ac0200f0
Jardel <jardel1101@gmail.com>
2021-01-07 23:12:32 -0300

    coupon relationship

---
+++ ecommerce-adonis/database/migrations/1610071164624_order_item_schema.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class OrderItemSchema extends Schema {
+  up () {
+    this.create('order_items', (table) => {
+      table.increments()
+      table.integer('order_id').unsigned()
+      table.integer('product_id').unsigned()
+      table.integer('quantity').unsigned()
+      table.decimal('subtotal', 12, 2)
+
+      table.foreign('image_id').references('id').inTable('images').onDelete('cascade')
+      table.foreign('order_id').references('id').inTable('orders').onDelete('cascade')
+    })
+  }
+
+  down () {
+    this.drop('order_items')
+  }
+}
+
+module.exports = OrderItemSchema
