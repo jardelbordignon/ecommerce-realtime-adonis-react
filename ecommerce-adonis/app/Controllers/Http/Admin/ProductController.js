0ceb6eff9784a96ed5b0a963b936770a6c9b73a3
Jardel <jardel1101@gmail.com>
2021-01-12 10:27:40 -0300

    Product Controller methods

--- ecommerce-adonis/app/Controllers/Http/Admin/ProductController.js
+++ ecommerce-adonis/app/Controllers/Http/Admin/ProductController.js
@@ -1,92 +1,62 @@
 'use strict'
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
-
-/**
- * Resourceful controller for interacting with products
- */
 class ProductController {
-  /**
-   * Show a list of all products.
-   * GET products
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index ({ request, response, view }) {
+
+  async index ({ request, response, pagination }) {
+    const name = request.input('name')
+    const query = Product.query()
+
+    if (name)
+      query.where('name', 'LIKE', `%${name}%`)
+
+    const products = await query.paginate(pagination.page, pagination.perPage)
+
+    return response.send(products)
   }
 
-  /**
-   * Render a form to be used for creating a new product.
-   * GET products/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
-  }
 
-  /**
-   * Create/save a new product.
-   * POST products
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
   async store ({ request, response }) {
+    try {
+      const { name, description, price, image_id } = request.all()
+      const product = await Product.create({ name, description, price, image_id })
+      return response.status(201).send(product)
+
+    } catch (error) {
+      return response.status(400).send({ message: 'Erro ao registrar o produto' })
+    }
   }
 
-  /**
-   * Display a single product.
-   * GET products/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async show ({ params, request, response, view }) {
+
+  async show ({ params, request, response }) {
+    const product = await Product.findOrFail(params.id)
+    return response.send(product)
   }
 
-  /**
-   * Render a form to update an existing product.
-   * GET products/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
 
-  /**
-   * Update product details.
-   * PUT or PATCH products/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
   async update ({ params, request, response }) {
+    try {
+      const product = await Product.findOrFail(params.id)
+      const { name, description, price, image_id } = request.all()
+
+      product.merge({ name, description, price, image_id })
+      await product.save()
+
+      return response.send(product)
+
+    } catch (error) {
+      return response.status(400).send({ message: 'Erro ao alterar o produto' })
+    }
   }
 
-  /**
-   * Delete a product with id.
-   * DELETE products/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
+
   async destroy ({ params, request, response }) {
+    const product = await Product.findOrFail(params.id)
+    try {
+      await product.delete()
+      return response.status(204).send()
+    } catch (error) {
+      return response.status(400).send({ message: 'Erro ao deletar o produto' })
+    }
   }
 }
 
