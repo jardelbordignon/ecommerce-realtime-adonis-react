237aaf54b8322bb889094528621a08d63783d38d
Jardel <jardel1101@gmail.com>
2021-01-13 21:59:58 -0300

    Add discount routes

--- ecommerce-adonis/start/routes/admin.js
+++ ecommerce-adonis/start/routes/admin.js
@@ -4,14 +4,27 @@
 const Route = use('Route')
 
 Route.group(() => {
-
   // resource routes (crud)
+
+  // Category
   Route.resource('categories', 'CategoryController').apiOnly()
-  Route.resource('coupons',    'CouponController').apiOnly()
-  Route.resource('images',     'ImageController').apiOnly()
-  Route.resource('order',      'OrderController').apiOnly()
-  Route.resource('products',   'ProductController').apiOnly()
-  Route.resource('users',      'UserController').apiOnly()
+
+  // Coupon
+  Route.resource('coupons', 'CouponController').apiOnly()
+
+  // Image
+  Route.resource('images', 'ImageController').apiOnly()
+
+  // Order
+  Route.post('orders/:id/discount', 'OrderController.applyDiscount')
+  Route.delete('orders/:id/discount', 'OrderController.removeDiscount')
+  Route.resource('order', 'OrderController').apiOnly()
+
+  // Product
+  Route.resource('products', 'ProductController').apiOnly()
+
+  // User
+  Route.resource('users', 'UserController').apiOnly()
 })
 .prefix('v1/admin')
 .namespace('Admin')
